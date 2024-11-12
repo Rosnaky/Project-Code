fdfc2f7d9b1e8a57269754413b57f643a73941a3
Haris Khawja <hariskhawja@gmail.com>
2024-11-12 14:42:54 -0500

    program to function

--- src/screenshot.py
+++ src/screenshot.py
@@ -13,45 +13,47 @@ Dependencies:
 - pip install keyboard
 """
 
+# file_path = "C:\\Users\\haris\\OneDrive\\Desktop\\SE101\\se101-f2024-triangles\\triangle.c"
+
 # stops program if more 1 minute is taken 
 MAX_ELAPSE_TIME = 60
 
 # get size of screen
-monitor = get_monitors()[0]
-width = monitor.width
-height = monitor.height
+monitor = pyautogui.size()
+width = monitor[0]
+height = monitor[1]
 print(f"Screen resolution: {width}x{height}")
+
 def scroll(height):
     pyautogui.scroll(height)
-
-
-
-file_path = "C:\\Users\\zroy1\\SE101\\se101-f2024-triangles\\triangle.c"
-os.system(f'code {file_path}')
-start_time = time.time()
-
 
 def image_to_bytes(image):
     return list(image.getdata())
 
-count = 1
-prev_screenshot = None
-while time.time() - start_time < MAX_ELAPSE_TIME:
-    print(count)
-    # fail safe, stops program if space bar is pressed 
-    if keyboard.is_pressed('ctrl'):
-        break
+def screenshotCapture(file_path):
+    os.system(f'code {file_path}')
+    start_time = time.time()
+    count = 1
+    prev_screenshot = None
+    time.sleep(5)
+    while time.time() - start_time < MAX_ELAPSE_TIME:
+        print(count)
+        # fail safe, stops program if space bar is pressed 
+        if keyboard.is_pressed('ctrl'):
+            break
 
-    # take screenshot 
-    screenshot = ImageGrab.grab(bbox =(0.04 * width, 0.075 * height, 0.85 * width, 0.95 * height))
-    # if the two screenshots are the same that means we've reached the end of our scrollable area 
-    if image_to_bytes(screenshot) == prev_screenshot:
-        print("stopped")
-        break
+        # take screenshot 
+        screenshot = ImageGrab.grab(bbox =(0.04 * width, 0.075 * height, 0.85 * width, 0.95 * height))
+        # if the two screenshots are the same that means we've reached the end of our scrollable area 
+        if image_to_bytes(screenshot) == prev_screenshot:
+            print("stopped")
+            break
 
-    prev_screenshot = image_to_bytes(screenshot)
-    img_path = f"..\\screenshots\\pic{count}.jpg"
-    screenshot.save(img_path)
-    scroll(-height)
-    count += 1
-    time.sleep(0.1)
+        prev_screenshot = image_to_bytes(screenshot)
+        img_path = f"..\\screenshots\\pic{count}.jpg"
+        screenshot.save(img_path)
+        scroll(-height)
+        count += 1
+        time.sleep(0.5)
+
+# screenshotCapture("C:\\Users\\haris\\OneDrive\\Desktop\\SE101\\se101-f2024-triangles\\triangle.c")
